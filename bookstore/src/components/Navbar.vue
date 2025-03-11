ad49b7710ff689f69d0c1cd3f42dd9b8ae7691a9
Imam Akbar <62373372+imaamaakbar@users.noreply.github.com>
2025-03-11 20:14:33 +0700

    first commit

--- bookstore/src/components/Navbar.vue
+++ bookstore/src/components/Navbar.vue
@@ -1,10 +1,34 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 <template>
-    <div>
-  <b-nav>
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
-  </b-nav>
-</div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link" :class="{ active: isActive('/') }" aria-current="page">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="cart" class="nav-link" :class="{ active: isActive('/cart') }">Cart</RouterLink>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    isActive(route) {
+      return this.$route.path === route;
+    },
+  },
+};
+</script>
